486eae320bad5d59d1b70bc1c7cfd2d15050617b
plyushkahot <plyxsa2004@gmail.com>
2023-05-11 13:27:54 +0300

    Added files

--- js/contacts.js
+++ js/contacts.js
@@ -1,13 +1,9 @@
-// Создаем новый объект XMLHttpRequest
 var xhttp = new XMLHttpRequest();
-// Открываем файл footer.xml
 xhttp.open("GET", "xml/shops.xml", true);
-// Когда файл загружен
+
 xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    // Заменяем пустой футер данными из XML файла
+  if (this.readyState == 4 && this.status == 200) {  
     document.getElementById("shop").innerHTML = this.responseText;
   }
 };
-// Загружаем файл
 xhttp.send()
